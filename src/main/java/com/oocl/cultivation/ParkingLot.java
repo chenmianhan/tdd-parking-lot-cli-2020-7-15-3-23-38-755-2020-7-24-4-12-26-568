dec8238858000a7c59b465bd3cbf2f955ebb2617
CHENJA28 <CHENJA28@OOCL.COM>
2020-07-24 16:18:44 +0800

    [parkingLot][James Chen] feature: implement should_return_null_ticket_when_park_car_given_parking_lot_full_capacity test

--- src/main/java/com/oocl/cultivation/ParkingLot.java
+++ src/main/java/com/oocl/cultivation/ParkingLot.java
@@ -6,7 +6,9 @@ import java.util.Map;
 
 public class ParkingLot {
     private final Map<Ticket,Car> parkingMap=new HashMap<>();
+    private int capacity=10;
     public Ticket park(Car car) {
+        if(capacity<=parkingMap.size())return null;
         Ticket carTicket=new Ticket();
         parkingMap.put(carTicket,car);
         return carTicket;
