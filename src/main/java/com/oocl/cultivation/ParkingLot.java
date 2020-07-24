4168d904787d3029f9e4a11bf52f80464794bfc9
CHENJA28 <CHENJA28@OOCL.COM>
2020-07-24 15:38:51 +0800

    [parkingLot][James Chen] feature: implement should_fetch_car_when_fetching_car_given_ticket test

--- src/main/java/com/oocl/cultivation/ParkingLot.java
+++ src/main/java/com/oocl/cultivation/ParkingLot.java
@@ -1,14 +1,18 @@
 package com.oocl.cultivation;
 
 
-
+import java.util.HashMap;
+import java.util.Map;
 
 public class ParkingLot {
+    private final Map<Ticket,Car> parkingMap=new HashMap<>();
     public Ticket park(Car car) {
-        return new Ticket();
+        Ticket carTicket=new Ticket();
+        parkingMap.put(carTicket,car);
+        return carTicket;
     }
 
     public Car fetch(Ticket carTicket) {
-        return null;
+        return parkingMap.get(carTicket);
     }
 }
