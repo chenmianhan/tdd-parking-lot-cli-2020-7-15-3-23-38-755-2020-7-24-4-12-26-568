29dcbdc1378703cb65a73c4f7981c0a8559eb43c
CHENJA28 <CHENJA28@OOCL.COM>
2020-07-24 15:57:32 +0800

    [parkingLot][James Chen] feature: add should_fetch_correspond_ticket_when_fetch_car_given_correspond_ticket test

--- src/test/java/com/oocl/cultivation/test/ParkingLotTests.java
+++ src/test/java/com/oocl/cultivation/test/ParkingLotTests.java
@@ -6,7 +6,8 @@ import com.oocl.cultivation.Ticket;
 import org.junit.jupiter.api.Test;
 
 
-import java.awt.*;
+import java.util.LinkedList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -28,16 +29,41 @@ public class ParkingLotTests {
     @Test
     void should_fetch_car_when_fetching_car_given_ticket() {
         //given
-
         ParkingLot parkingLot = new ParkingLot();
         Car parkedCar = new Car();
         Ticket carTicket = parkingLot.park(parkedCar);
+
         //when
-
         Car fetchedCar = parkingLot.fetch(carTicket);
-        //then
 
+        //then
         assertNotNull(fetchedCar);
         assertEquals(fetchedCar, parkedCar);
+    }
+
+    @Test
+    void should_fetch_correspond_ticket_when_fetch_car_given_correspond_ticket() {
+        //given
+        ParkingLot parkingLot = new ParkingLot();
+        List<Car> parkedCars = new LinkedList<>();
+        List<Ticket> tickets = new LinkedList<>();
+        int carNumber = 10;
+        for (int i = 0; i < carNumber; i++) {
+            parkedCars.add(new Car());
+        }
+
+        //when
+        for (Car car : parkedCars) {
+            tickets.add(parkingLot.park(car));
+        }
+        List<Car> fetchedCars = new LinkedList<>();
+        for (Ticket ticket : tickets) {
+            fetchedCars.add(parkingLot.fetch(ticket));
+        }
+
+        //then
+        for (int i = 0; i <carNumber; i++) {
+            assertEquals(fetchedCars.get(i),parkedCars.get(i));
+        }
     }
 }
