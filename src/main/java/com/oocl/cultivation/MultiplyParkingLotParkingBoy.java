2cffa4c8083681f9f904d0e70cbf424e8de41a46
chenmianhan <mianhan1998@139.com>
2020-07-26 00:02:53 +0800

    [ParkingLot][James Chen] refactor: implement should_return_please_provide_your_parking_ticket_when_fetch_car_then_no_provide_the_ticket_and_query_message test

--- src/main/java/com/oocl/cultivation/MultiplyParkingLotParkingBoy.java
+++ src/main/java/com/oocl/cultivation/MultiplyParkingLotParkingBoy.java
@@ -3,15 +3,22 @@ package com.oocl.cultivation;
 import java.util.List;
 
 public class MultiplyParkingLotParkingBoy {
+    private final MessageRecord errorMessageRecord;
+    private final List<ParkingLot> parkingLots;
     public MultiplyParkingLotParkingBoy(List<ParkingLot> parkingLots) {
-
+        errorMessageRecord=new ErrorMessageRecord();
+        this.parkingLots=parkingLots;
     }
 
     public Car fetch(Ticket ticket) {
+        if(ticket==null){
+            errorMessageRecord.setMessage("Please provide your parking ticket.");
+
+        }
         return null;
     }
 
     public String getErrorMessage() {
-        return null;
+        return errorMessageRecord.getMessage();
     }
 }
