bcc636ff01f72d1a1d9f2f86fd724d685572e9a9
chenmianhan <mianhan1998@139.com>
2020-07-26 15:50:48 +0800

    [ParkingLot][James Chen] feature: add should_fetch_correspond_ticket_when_fetch_car_given_correspond_ticket test

--- src/test/java/com/oocl/cultivation/test/SuperSmartParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/test/SuperSmartParkingBoyTest.java
@@ -53,5 +53,35 @@ class SuperSmartParkingBoyTest {
         assertNotNull(fetchedCar);
         assertEquals(fetchedCar, parkedCar);
     }
+
+    @Test
+    void should_fetch_correspond_ticket_when_fetch_car_given_correspond_ticket() {
+        //given
+        List<ParkingLot> parkingLots = new LinkedList<>();
+        parkingLots.add(new ParkingLot());
+        parkingLots.add(new ParkingLot());
+        SuperSmartParkingBoy superSmartParkingBoy = new SuperSmartParkingBoy(parkingLots);
+        List<Car> parkedCars = new LinkedList<>();
+        List<Ticket> tickets = new LinkedList<>();
+        int carNumber = 12;
+        for (int i = 0; i < carNumber; i++) {
+            parkedCars.add(new Car());
+        }
+
+        //when
+        for (Car car : parkedCars) {
+            tickets.add(superSmartParkingBoy.park(car));
+        }
+        List<Car> fetchedCars = new LinkedList<>();
+        for (Ticket ticket : tickets) {
+            fetchedCars.add(superSmartParkingBoy.fetch(ticket));
+        }
+
+        //then
+        for (int i = 0; i < carNumber; i++) {
+            assertEquals(fetchedCars.get(i), parkedCars.get(i));
+        }
+
+    }
 }
 
