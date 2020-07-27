5d2d1a0a80ac4038cc3e4ba341e503245aaa044e
CHENJA28 <CHENJA28@OOCL.COM>
2020-07-27 18:33:47 +0800

    [parkingLot][James Chen] refactor: add two new exception

---
+++ src/main/java/com/oocl/cultivation/FetchException.java
@@ -0,0 +1,7 @@
+package com.oocl.cultivation;
+
+public class FetchException extends Exception {
+    public FetchException(String exceptionMessage){
+        super(exceptionMessage);
+    }
+}
---
+++ src/main/java/com/oocl/cultivation/ParkingException.java
@@ -0,0 +1,8 @@
+package com.oocl.cultivation;
+
+public class ParkingException extends Exception {
+    public ParkingException(String exceptionMessage){
+        super(exceptionMessage);
+    }
+
+}
