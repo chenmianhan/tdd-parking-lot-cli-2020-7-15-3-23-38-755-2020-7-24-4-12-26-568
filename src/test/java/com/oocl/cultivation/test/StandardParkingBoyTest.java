f45121e2d563dd7aed37e9027d7879f7ecefc0d9
CHENJA28 <CHENJA28@OOCL.COM>
2020-07-27 17:04:53 +0800

    [parkingLot][James Chen] refactor: merge the test in the MultiplyParkingLotParkingBoyTest into the StandardParkingBoyTest

--- src/test/java/com/oocl/cultivation/test/StandardParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/test/StandardParkingBoyTest.java
@@ -1,10 +1,7 @@
 package com.oocl.cultivation.test;
 
 
-import com.oocl.cultivation.Car;
-import com.oocl.cultivation.ParkingLot;
-import com.oocl.cultivation.StandardParkingBoy;
-import com.oocl.cultivation.Ticket;
+import com.oocl.cultivation.*;
 import org.junit.jupiter.api.Test;
 
 import java.util.LinkedList;
@@ -67,6 +64,7 @@ class StandardParkingBoyTest {
         assertNotNull(fetchedCar);
         assertEquals(fetchedCar, parkedCar);
     }
+
     @Test
     void should_fetch_correspond_ticket_when_fetch_car_given_correspond_ticket() {
         //given
@@ -96,6 +94,7 @@ class StandardParkingBoyTest {
         }
 
     }
+
     @Test
     void should_fetch_null_car_when_fetch_given_a_wrong_ticket() {
         //given
@@ -129,6 +128,72 @@ class StandardParkingBoyTest {
         //then
         assertNull(fetchAgainCar);
         assertEquals("Unrecognized park ticket.", standardParkingBoy.getErrorMessage());
+    }
+
+    @Test
+    void should_fetch_null_car_when_fetch_given_no_ticket() {
+        //given
+        List<ParkingLot> parkingLots = new LinkedList<>();
+        MultiplyParkingLotParkingBoy multiplyParkingLotParkingBoy = new MultiplyParkingLotParkingBoy(parkingLots);
+
+        //when
+        Car car = multiplyParkingLotParkingBoy.fetch(null);
+
+        //then
+        assertNull(car);
+    }
+
+    @Test
+    void should_return_please_provide_your_parking_ticket_when_fetch_car_then_no_provide_the_ticket_and_query_message() {
+        //given
+        List<ParkingLot> parkingLots = new LinkedList<>();
+        parkingLots.add(new ParkingLot());
+        parkingLots.add(new ParkingLot());
+        MultiplyParkingLotParkingBoy multiplyParkingLotParkingBoy = new MultiplyParkingLotParkingBoy(parkingLots);
+
+        //when
+        multiplyParkingLotParkingBoy.fetch(null);
+
+        //then
+        assertEquals("Please provide your park ticket.", multiplyParkingLotParkingBoy.getErrorMessage());
+    }
+
+    @Test
+    void should_return_not_enough_position_when_park_car_then_parking_lot_full_and_query_message() {
+        //given
+        int capacity = 10;
+        List<ParkingLot> parkingLots = new LinkedList<>();
+        ParkingLot parkingLot1 = new ParkingLot();
+        ParkingLot parkingLot2 = new ParkingLot();
+        for (int i = 0; i < capacity; i++) {
+            parkingLot1.park(new Car());
+            parkingLot2.park(new Car());
+        }
+        parkingLots.add(parkingLot1);
+        parkingLots.add(parkingLot2);
+        MultiplyParkingLotParkingBoy multiplyParkingLotParkingBoy = new MultiplyParkingLotParkingBoy(parkingLots);
+
+        //when
+        multiplyParkingLotParkingBoy.park(new Car());
+
+        //then
+        assertEquals("Not enough position.", multiplyParkingLotParkingBoy.getErrorMessage());
+    }
+
+    @Test
+    void should_return_unrecognized_parking_ticket_when_fetch_car_then_not_provide_the_right_ticket_and_query_message() {
+        //given
+        List<ParkingLot> parkingLots = new LinkedList<>();
+        parkingLots.add(new ParkingLot());
+        parkingLots.add(new ParkingLot());
+        MultiplyParkingLotParkingBoy multiplyParkingLotParkingBoy = new MultiplyParkingLotParkingBoy(parkingLots);
+
+        //when
+        multiplyParkingLotParkingBoy.park(new Car());
+        multiplyParkingLotParkingBoy.fetch(new Ticket());
+
+        //then
+        assertEquals("Unrecognized park ticket.", multiplyParkingLotParkingBoy.getErrorMessage());
     }
 
 }
